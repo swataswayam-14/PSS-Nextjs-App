9ae27d51f069767705dac019046613829809f608
Swata Swayam Dash <swataswayamdash@gmail.com>
2024-04-13 21:46:35 +0530

    UI improvements

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -25,12 +25,8 @@ export default function Home() {
       <CopyRight/>
       <div className="bg-snow fixed bottom-2 right-0 z-50 rounded-full shadow-lg hover:bg-gray-100 transition-colors duration-300">
         <a href="https://wa.me/9007881973?text=Hello can I help you?" target="_blank">
-          <div className="flex flex-col sm:p-2 md:p-4 p-1 hover:bg-yellow-200 bg-snow rounded-full items-center">
           <img src="../whatsapp.png" alt="WhatsApp" className="w-14 h-14 object-cover rounded-lg" />
-          <h1 className="text-sm font-extrabold text-black ">
-            Connect with us
-          </h1>
-          </div>
+          
         </a>
         </div>
     </div>
